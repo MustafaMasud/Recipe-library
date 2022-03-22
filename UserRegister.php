61798f78152a100a303c3b4ea075827d7e7a23a1
Mustafa Mohsin <mustafa.mohsiny@gmail.com>
2022-03-22 14:09:09 -0400

    Added Register functionality with SQL DB

---
+++ UserRegister.php
@@ -0,0 +1,33 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "your password";
+    $dbname="myDB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // sql to create table
+
+    
+    //prepare and bind
+    $stmt = $conn->prepare("INSERT INTO ListMembers (username, email, pass) VALUES ( ?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $pass);
+    
+     // set parameters and execute
+     $username = $_POST['name'];
+     $email = $_POST['email'];
+     $pass = $_POST['psw'];
+    $stmt->execute();
+
+    echo "You're successfully registered";
+
+    $stmt->close();
+    $conn->close();
+?>
--- register.php
+++ register.php
@@ -31,14 +31,17 @@
 
 
     <section>
-        <form action="register.php">
+        <form action="UserRegister.php" method="POST">
             <link rel="stylesheet" href="style.css">
             <div class="container">
                 <h1>Register</h1>
                 <p>Please fill in this form to create an account.</p>
                 <hr>
 
-                <label for="email"><b>Email</b></label>
+                <label for="name"><b>Name</b></label>
+                <input type="text" placeholder="Enter Name" name="name" id="name" required>
+
+                <label for="email"><b>Emaili</b></label>
                 <input type="text" placeholder="Enter Email" name="email" id="email" required>
 
                 <label for="psw"><b>Password</b></label>
